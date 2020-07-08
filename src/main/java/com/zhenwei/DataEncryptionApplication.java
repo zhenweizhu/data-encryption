0517d831dff3f274479cf4c4349bb966b1a20a5a
zhuzhenwei <861380255@qq.com>
2020-07-08 15:55:36 +0800

    新建工程

---
+++ src/main/java/com/zhenwei/DataEncryptionApplication.java
@@ -0,0 +1,11 @@
+package com.zhenwei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataEncryptionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DataEncryptionApplication.class,args);
+    }
+}
